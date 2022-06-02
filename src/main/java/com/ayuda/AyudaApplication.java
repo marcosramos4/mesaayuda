063386f28f17f11ba1789f3f775c542f1c8d5f73
Marcos Ramos <marcos-ramos-4@hotmail.com>
2022-06-01 19:20:39 -0500

    Avance calidad TI

---
+++ src/main/java/com/ayuda/AyudaApplication.java
@@ -0,0 +1,13 @@
+package com.ayuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyudaApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/static/assets/script.js
@@ -0,0 +1,10 @@
+function ensima(vnt){
+	var g=document.getElementById(vnt);
+	g.style.display="block";
+	
+}
+function oculta(vnt){
+	var g=document.getElementById(vnt);
+	g.style.display="";
+	
+}
